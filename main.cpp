f956011d9be23f138c9c27eb7090dae336e54903
amir mohammad <amirmohammadrezvaninia@gmail.com>
2021-08-26 22:44:19 +0430

    get input data from the user

--- main.cpp
+++ main.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main() {
-    
+
+    int row,col;
+
+    cout<<"Input the row: ";            cin>>row;
+    cout<<"Input the column: ";         cin>>col; 
+
+    if( row == col ) {
+        
+        bool M[row][col];
+
+        cout<<endl<<"---Input the boolean matrix "<<row<<"x"<<col<<"---"<<endl;
+        
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                cin>>M[i][j];
+    }
+
+    else {
+        cout<<"Error: The matrix is not square"<<endl;
+    }
+
     return 0;
 }
